2f4e2ed6b124169d1cc306b83365c3ac2838227c
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-23 14:38:18 +0700

    perfect

--- EmikoRobot/modules/sql/users_sql.py
+++ EmikoRobot/modules/sql/users_sql.py
@@ -7,6 +7,7 @@ from sqlalchemy import (
     ForeignKey,
     String,
     UnicodeText,
+    Integer
     UniqueConstraint,
     func,
 )
@@ -41,7 +42,7 @@ class Chats(BASE):
 
 class ChatMembers(BASE):
     __tablename__ = "chat_members"
-    priv_chat_id = Column(BigInteger, primary_key=True)
+    priv_chat_id = Column(Integer, primary_key=True)
     # NOTE: Use dual primary key instead of private primary key?
     chat = Column(
         String(14),
