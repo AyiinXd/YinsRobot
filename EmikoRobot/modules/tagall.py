385199eb845849d057a3b5f612d646ac11812ddd
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2022-01-03 12:19:29 +0700

    nothing

--- EmikoRobot/modules/tagall.py
+++ EmikoRobot/modules/tagall.py
@@ -1,37 +1,122 @@
 import asyncio
+
 from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from EmikoRobot import telethn
-from EmikoRobot.events import register as tomori
+from telethon.errors import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.types import ChannelParticipantCreator
+
+from EmikoRobot import telethn as client
+
+spam_chats = []
 
 
-@tomori(pattern="^/tagall ?(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "Hi Friends I'm Emiko I Call To All Of You"
-    chat = await event.get_input_chat()
-    async for x in telethn.iter_participants(chat, 100):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    await event.reply(mentions)
-    await event.delete()
+@client.on(events.NewMessage(pattern="^/tagall|@all|/all ?(.*)"))
+async def mentionall(event):
+    chat_id = event.chat_id
+    if event.is_private:
+        return await event.respond("__This command can be use in groups and channels!__")
 
-
-@tomori(pattern="^/users ?(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "Users : "
-    chat = await event.get_input_chat()
-    async for x in telethn.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
+    is_admin = False
+    try:
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
+    except UserNotParticipantError:
+        is_admin = False
     else:
-        await event.reply(mentions)
-    await event.delete()
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.reply("__Only admins can mention all!__")
+
+    if event.pattern_match.group(1) and event.is_reply:
+        return await event.reply("__Give me one argument!__")
+    elif event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.is_reply:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg == None:
+            return await event.respond(
+                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
+    else:
+        return await event.reply("__Reply to a message or give me some text to mention others!__")
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ''
+    async for usr in client.iter_participants(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        if usrnum == 5:
+            if mode == "text_on_cmd":
+                txt = f"{msg}\n{usrtxt}"
+                await client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(usrtxt)
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ''
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
 
 
-__mod_name__ = "Tagger"
+@client.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+    is_admin = False
+    try:
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.reply("__Only admins can execute this command!__")
+    if not event.chat_id in spam_chats:
+        return await event.reply("__There is no proccess on going...__")
+    else:
+        try:
+            spam_chats.remove(event.chat_id)
+        except:
+            pass
+        return await event.respond("__Stopped Mention.__")
+
+
+__mod_name__ = "Tag all"
+__help__ = """
+──「 Mention all func 」──
+
+Aika Can Be a Mention Bot for your group.
+
+Only admins can tag all.  here is a list of commands
+
+❂ /tagall or @all (reply to message or add another message) To mention all members in your group, without exception.
+❂ /cancel for canceling the mention-all.
+"""
