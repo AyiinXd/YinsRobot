2424cdcb44b1ecb435263013dcd8963b7165695d
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-01-05 22:31:08 +0700

    Using `dispatcher.bot.first_name`

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -229,7 +229,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            f"👋 Hi, I'm Emiko. Nice to meet You.",
+            f"👋 Hi, I'm {dispatcher.bot.first_name}. Nice to meet You.",
             parse_mode=ParseMode.HTML
        )
 
