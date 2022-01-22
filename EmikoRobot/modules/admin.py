5f8b7eccb7938d643406d0d92185b1f992e93479
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2022-01-22 19:55:35 +0700

    ok

--- EmikoRobot/modules/admin.py
+++ EmikoRobot/modules/admin.py
@@ -212,7 +212,7 @@ def promote(update: Update, context: CallbackContext) -> str:
             can_edit_messages=bot_member.can_edit_messages,
             can_delete_messages=bot_member.can_delete_messages,
             can_invite_users=bot_member.can_invite_users,
-            # can_promote_members=bot_member.can_promote_members,
+            can_manage_voice_chats=bot_member.can_manage_voice_chats,
             can_restrict_members=bot_member.can_restrict_members,
             can_pin_messages=bot_member.can_pin_messages,
         )
